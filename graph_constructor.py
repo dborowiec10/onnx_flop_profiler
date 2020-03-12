150f5065eb76a5fa68f10db2e7c2ec0ce693c018
Damian Borowiec <dborowiec10@gmail.com>
2020-03-12 02:05:04 +0000

    new

--- graph_constructor.py
+++ graph_constructor.py
@@ -1,6 +1,36 @@
 import parser
 from pprint import pprint
+import networkx as nx
+import matplotlib.pyplot as plt
 
-out = parser.parse("imagenet_nasnet_large.graph")
+G = nx.Graph()
 
-pprint(out)
+
+def find_op_by_output_name(out_name, ops):
+    for o in reversed(ops):
+        if o["output"]["name"] == out_name:
+            return o
+    return None
+
+
+# plt.subplot(121)
+# nx.draw(G, with_labels=True, font_weight='bold')
+# plt.show()
+
+
+out = parser.parse("imagenet_vgg16.graph")
+
+G.add_node("Output", name=out["return"], type="variable")
+o = find_op_by_output_name(G.nodes["Output"]["name"], out["operations"])
+print(o)
+G.add_node(o["operation"], arguments=o["arguments"])
+
+print(G.nodes["Output"])
+
+
+# for ops in reversed(out["operations"]):
+
+
+
+
+# pprint(out)
