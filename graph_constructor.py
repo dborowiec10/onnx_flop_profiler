2fda6a2fe6dfbc8fc76097357475ead7b45b54e1
Damian Borowiec <dborowiec10@gmail.com>
2020-03-12 02:57:43 +0000

    all my work was unnecessary...

--- graph_constructor.py
+++ graph_constructor.py
@@ -1,36 +1,61 @@
-import parser
+import onnx
 from pprint import pprint
-import networkx as nx
-import matplotlib.pyplot as plt
+from onnx import ModelProto
 
-G = nx.Graph()
-
-
-def find_op_by_output_name(out_name, ops):
-    for o in reversed(ops):
-        if o["output"]["name"] == out_name:
-            return o
-    return None
+mod = onnx.load("imagenet_mobilenet_alpha_0.25.onnx")
+for node in mod.graph.node:
+    # print(node.input)
+    print(node.output)
+    print(node.op_type)
+    print(node)
 
 
-# plt.subplot(121)
-# nx.draw(G, with_labels=True, font_weight='bold')
-# plt.show()
+for init in mod.graph.initializer:
+    continue
+    # print(init.dims)
+    # print(init.name)
+
+for inp in mod.graph.input:
+    print(inp)
+
+for inp in mod.graph.output:
+    print(inp)
+
+# pprint(mod)
+
+# import parser
+# from pprint import pprint
+# import networkx as nx
+# import matplotlib.pyplot as plt
+
+# G = nx.Graph()
 
 
-out = parser.parse("imagenet_vgg16.graph")
-
-G.add_node("Output", name=out["return"], type="variable")
-o = find_op_by_output_name(G.nodes["Output"]["name"], out["operations"])
-print(o)
-G.add_node(o["operation"], arguments=o["arguments"])
-
-print(G.nodes["Output"])
+# def find_op_by_output_name(out_name, ops):
+#     for o in reversed(ops):
+#         if o["output"]["name"] == out_name:
+#             return o
+#     return None
 
 
-# for ops in reversed(out["operations"]):
+# # plt.subplot(121)
+# # nx.draw(G, with_labels=True, font_weight='bold')
+# # plt.show()
+
+
+# out = parser.parse("imagenet_vgg16.graph")
+
+# G.add_node("Output", name=out["return"], type="variable")
+# o = find_op_by_output_name(G.nodes["Output"]["name"], out["operations"])
+# print(o)
+# G.add_node(o["operation"], arguments=o["arguments"])
+
+# print(G.nodes["Output"])
+
+
+# # for ops in reversed(out["operations"]):
 
 
 
 
-# pprint(out)
+# # pprint(out)
